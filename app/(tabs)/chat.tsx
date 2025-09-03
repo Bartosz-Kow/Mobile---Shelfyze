d164a1182a3b2af98fa5849665d66b74876201a7
Bartosz Kowynia <164752835+Bartosz-Kow@users.noreply.github.com>
2025-09-03 18:15:24 +0200

    Add routing after register to HomeScreen (tabs)

---
+++ app/(tabs)/chat.tsx
@@ -0,0 +1,5 @@
+import ChatScreen from "@/src/screens/ChatTemplate";
+
+export default function Home() {
+  return <ChatScreen />;
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,5 @@
+import HomeScreen from "@/src/screens/HomeTemplate";
+
+export default function Home() {
+  return <HomeScreen />;
+}
